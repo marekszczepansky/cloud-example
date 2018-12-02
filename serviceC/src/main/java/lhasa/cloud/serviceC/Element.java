8f5b439e31ce27577e896db03d9e30aadd5f3851
marekszczepansky <marek.szczepansky@gmail.com>
2018-12-02 23:09:51 +0100

    initial micro services version

---
+++ serviceC/src/main/java/lhasa/cloud/serviceC/Element.java
@@ -0,0 +1,40 @@
+package lhasa.cloud.serviceC;
+
+public class Element {
+    private String sourceService = "Service C";
+    private int sourcePort;
+    private String message;
+
+    public Element(String message) {
+        this.message = message;
+    }
+
+    public Element(String message, Integer port) {
+        this.message = message;
+        this.sourcePort = port;
+    }
+
+    public String getSourceService() {
+        return sourceService;
+    }
+
+    public void setSourceService(String sourceService) {
+        this.sourceService = sourceService;
+    }
+
+    public int getSourcePort() {
+        return sourcePort;
+    }
+
+    public void setSourcePort(int sourcePort) {
+        this.sourcePort = sourcePort;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
